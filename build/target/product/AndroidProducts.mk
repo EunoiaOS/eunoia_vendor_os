daf6a5512be57300718f681668ded336083152ba
EunoiaOS <235215905+eunoia-committer@users.noreply.github.com>
2025-12-03 06:24:00 +0700

    build: Welcome mido
    Change-Id: I99c5df97bdb0b389b991fd1c9ff6d41807c02334

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -16,10 +16,12 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/eunoia_dipper.mk \
+    $(LOCAL_DIR)/eunoia_mido.mk \
     $(LOCAL_DIR)/eunoia_rosy.mk \
     $(LOCAL_DIR)/eunoia_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
     eunoia_dipper-userdebug \
+    eunoia_mido-userdebug \
     eunoia_rosy-userdebug \
     eunoia_X00TD-userdebug
