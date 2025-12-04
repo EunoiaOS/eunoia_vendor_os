99dfb45d1f1f2328063e474c5c3e9b77d3b070d7
EunoiaOS <235215905+eunoia-committer@users.noreply.github.com>
2025-12-04 22:15:28 +0700

    config: Add FaceUnlock support
    Change-Id: I17f724bdc119f553bf66dabe4310e6f3565e6446

--- config/common.mk
+++ config/common.mk
@@ -136,6 +136,17 @@ endif
 PRODUCT_PACKAGES += \
     LineageSettingsProvider
 
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig \
