b02f96dcb2e728071f8bed9dd91784494fe140f3
EunoiaOS <235215905+eunoia-committer@users.noreply.github.com>
2026-01-23 14:16:33 +0700

    config: New Eunoia signing
    Change-Id: Iafe7950ed177eaac0d57328f2ec4521b0edf8c9f

--- config/signing.mk
+++ config/signing.mk
@@ -1,8 +1,11 @@
-EUNOIA_PATH := vendor/os/build/target/product
+EUNOIA_PATH := vendor/os/build/target/product/eunoia
+
+ifneq ($(wildcard $(EUNOIA_PATH)),)
 EUNOIA_PRODUCTS := $(basename $(notdir $(filter $(EUNOIA_PATH)/eunoia_%.mk, $(wildcard $(EUNOIA_PATH)/eunoia_*.mk))))
 
 ifneq ($(filter $(TARGET_PRODUCT),$(EUNOIA_PRODUCTS)),)
     EUNOIA_BUILDTYPE := VERIFIED
     PRODUCT_PACKAGES += Updater
     PRODUCT_COPY_FILES += vendor/os/prebuilt/common/etc/init/init.eunoia-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.eunoia-updater.rc
+endif
 endif
